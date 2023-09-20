e953ee3299ad203ed77bae72ac5ca692a1297f8e
pavan2962 <137707166+pavan2962@users.noreply.github.com>
2023-09-20 14:09:17 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:hotnfast@gmail.com">hotnfast@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>8AM-10PM</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Bengaluru, India-560064</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+  
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
